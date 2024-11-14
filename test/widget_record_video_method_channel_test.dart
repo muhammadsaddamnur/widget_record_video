04097964d051b1d9678b43098d994e84878fcafe
huydq <huydq@rocketstudio.com>
2024-11-14 15:33:15 +0700

    remove test channel

--- test/widget_record_video_method_channel_test.dart
+++ test/widget_record_video_method_channel_test.dart
@@ -1,29 +1,29 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:widget_record_video/src/widget_record_video_method_channel.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:widget_record_video/src/widget_record_video_method_channel.dart';
 
-void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
+// void main() {
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelWidgetRecordVideo platform = MethodChannelWidgetRecordVideo();
-  const MethodChannel channel = MethodChannel('widget_record_video');
+//   MethodChannelWidgetRecordVideo platform = MethodChannelWidgetRecordVideo();
+//   const MethodChannel channel = MethodChannel('widget_record_video');
 
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
+//   setUp(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+//         .setMockMethodCallHandler(
+//       channel,
+//       (MethodCall methodCall) async {
+//         return '42';
+//       },
+//     );
+//   });
 
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, null);
-  });
+//   tearDown(() {
+//     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+//         .setMockMethodCallHandler(channel, null);
+//   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
-}
+//   test('getPlatformVersion', () async {
+//     expect(await platform.getPlatformVersion(), '42');
+//   });
+// }
